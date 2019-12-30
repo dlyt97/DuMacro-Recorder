59a4443bd732562113cd8455b5c2d834e075f8d1
dulechannel97 <46886231+dulechannel97@users.noreply.github.com>
2019-12-30 21:19:41 +0100

    Update DuMacro Keyboard.py

--- DuMacro Keyboard.py
+++ DuMacro Keyboard.py
@@ -1,75 +1,69 @@
 #DuMacro RECORDER - KEYBOARD
 from pynput import keyboard
 import time
-x=input('File name:')
-macro = r''+x+'.py'
-fw=open(macro,'w')
-fw.write('from pynput.keyboard import Key, Controller\n\n')
-fw.write('import time\n\n')
-fw.write('keyboard = Controller()\n\n')
-fw.close()
-duTime_v2 = [] # PRESS <-> RELEASE
-duTime_v2_release = [] # RELEASE <-> PRESS 
-endd = [] # RELEASE <-> PRESS (TIME)
-VREME_KEY = []
+
+press_release=[]
+release_press=[]
+VREME_KEY=[]
+
+macro=r'DuKEY.py'
+
+def zapis(text,mode='a'):
+    f=open(macro,mode);f.write(text);f.close()
+zap='''from pynput.keyboard import Key, Controller
+import time
+keyboard = Controller()\n\n'''
+zapis(zap,'w')
+
 def on_press(key):
     try:
         if len(VREME_KEY) == 1:
-            str_vreme = 'time.sleep('+str(time.time() - VREME_KEY[0])+')\n'
-            zapis=open(macro,'a')
-            zapis.write(str_vreme)
-            zapis.close()
+            try:
+                zapis('time.sleep('+str(time.time() - VREME_KEY[0])+')\n')#WRITE
+            except:
+                print('NECE DA SE ZAPISE VREME ZA PRESS KEY !!!')
             VREME_KEY.clear()
-        jedan  = time.time()
-        duTime_v2_release.append(jedan)
+        release_press.append(time.time())
         try:
-            if len(duTime_v2_release) == 2:
-                string = str(duTime_v2_release[1]-duTime_v2_release[0])
-                str_time = '\ntime.sleep('+string+')\n'
-                f=open(macro,'a')
-                f.write(str_time)
-                f.close()
-                del duTime_v2_release[0]
+            if len(release_press) == 2:
+                try:
+                    zapis('\ntime.sleep('+str(release_press[1]-release_press[0])+')\n')#WRITE
+                except:
+                    print('NECE DA SE ZAPISE VREME ZA PRESS KEY & RELEASE KEY !!!')
+                del release_press[0]
         except Exception:
             print('NEMA')
-        txt_1 = "\nkeyboard.press('{0}')\n".format(key.char)
-        press_1 = txt_1
-        f=open(macro,'a')
-        f.write(press_1)
-        f.close()
-        start = time.time()
-        duTime_v2.append(start)   
+        zapis(str("\nkeyboard.press('{0}')\n".format(key.char)))#WRITE
+        press_release.append(time.time())
     except AttributeError:
-        txt_2 = "\nkeyboard.press({0})\n".format(key)
-        press_2 = txt_2
-        f=open(macro,'a')
-        f.write(press_2)
-        f.close()
-        start = time.time()
-        duTime_v2.append(start)
+        try:
+            zapis("\nkeyboard.press({0})\n".format(key))#WRITE
+        except:
+            print('NECE DA SE ZAPISE !!!')
+        press_release.append(time.time())
+        
 def on_release(key):
     try:
-        end = time.time()
-        duTime_v2.append(start)
-        str_end = duTime_v2[1] - duTime_v2[0]
-        time_sleep = '\ntime.sleep('+str(str_end)+')\n'
-        f=open(macro,'a')
-        f.write(time_sleep)
-        f.close()
-        duTime_v2.clear()
+        press_release.append(time.time());zapis(str('\ntime.sleep('+str(press_release[1] - press_release[0])+')\n'));press_release.clear()
     except Exception:
-        f=open(macro,'a')
-        f.write('\ntime.sleep(0.03)\n')
-        f.close()
-    txt_3 = "\nkeyboard.release({0})\n".format(key)
-    f=open(macro,'a')
-    f.write(txt_3)
-    f.close()
+        try:
+            zapis(str('\ntime.sleep(0.03)\n'))
+        except:
+            print('NECE DA SE ZAPISE 0.03 VREME !!!')
+    try:
+        zapis(str("\nkeyboard.release({0})\n".format(key)))
+    except:
+        print('NECE DA SE ZAPISE RELEASE KEY !!!')
     if key == keyboard.Key.esc:
         exit()
         return False
-def dc97():
+    
+def _keyboard_():
     with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
         listener.join()
-VREME_KEY.append(time.time())
-dc97()
+    
+def du_key():
+    VREME_KEY.append(time.time())
+    _keyboard_()
+    
+du_key()
